725faddefa4967e9ad75e5209619202fec5cfa89
skalteis <skalteis@users.noreply.github.com>
2023-05-21 03:09:51 +0200

    Add arg parser

--- fittest.py
+++ fittest.py
@@ -8,6 +8,49 @@ import io
 import statistics
 import time
 import re
+import sys
+import getopt
+
+port = "loop://"
+baudrate = 1200
+number = 8
+
+argv = sys.argv[1:]
+
+try:
+    opts, args = getopt.getopt(argv, "p:r:m:n:t",
+                               ["port =",
+                                "baudrate =",
+                                "mode =",
+                                "number =",
+                                "threshold ="])
+
+except:
+    print("Error")
+
+for opt, arg in opts:
+    if opt in ['-p', '--port']:
+        port = arg
+    elif opt in ['-r', '--baudrate']:
+        baudrate = arg
+    elif opt in ['-m', '--mode']:
+        mode = arg
+    elif opt in ['-n', '--number']:
+        number = arg
+    elif opt in ['-t', '--threshold']:
+        ff_pass_level = arg
+
+ff_pass_level = 100
+num_exercises = number
+if mode == "classic":
+    mode_osha_classic = True
+    mode_osha_modified = False
+    num_exercises = 8
+    ff_pass_level = 100
+if mode == "modified":
+    mode_osha_classic = False
+    mode_osha_modified = True
+    ff_pass_level = 100
 
 # defaults
 #ff_pass_level = 100
@@ -44,10 +87,10 @@ mode_osha_modified = False
 # fftotal = statistics.harmonic_mean(ff1, ff2, ..., ffn)
 
 # set port, instead of loop, devicenames such as COM3 or /dev/ttyUSB0 are also supported
-ser = serial.serial_for_url('loop://', timeout=1, do_not_open=True)
+ser = serial.serial_for_url(port, timeout=1, do_not_open=True)
 # PortaCount is baudrate 1200, no parity, 8 bit, 1 stopbit
 # hardware flow control (CTS) is non-required per DIP switch 8 (set to ON, confusingly) in the factory config
-ser.baudrate = 1200
+ser.baudrate = baudrate
 ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
 ser.bytesize = serial.EIGHTBITS
@@ -178,27 +221,194 @@ def ft_exercise(last_amb_conc=None, presample_ambient=False):
         result = [ffactor, conc_amb_post]
     return result
 
+def ft_osha_modified():
+    result = []
+    print("Exercise #1 (20 sec + 30 sec + purge time)")
+    # Switch to ambient inlet
+    print("Switching to ambient inlet port.")
+    sio.write("VN\r")
+    sio.flush()
+
+    # Ambient purge
+    print("Purging.")
+    time.sleep(amb_purge_time)
+    #t_end = time.monotonic() + amb_purge_time
+    #while time.monotonic() < t_end:
+    #    # do whatever you do
+
+    # Pre-mask ambient sample
+    print("Analyzing ambient air.")
+    t_end = time.monotonic() + 20
+    amb_particles_pre = 0
+    matcher="Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            amb_particles_pre = amb_particles_pre + float(x.lastgroup)
+
+
+    # Switch to mask inlet
+    print("Switching to sample inlet port.")
+    sio.write("VF\r")
+    sio.flush()
+
+    # Mask purge
+    print("Purging.")
+    time.sleep(mask_purge_time)
+    #t_end = time.monotonic() + mask_purge_time
+    #while time.monotonic() < t_end:
+    #    # do whatever you do
+
+    # Mask sample
+    print("Analyzing sample air.")
+    t_end = time.monotonic() + 30
+    mask_particles_1 = 0
+    matcher = "Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            mask_particles_1 = mask_particles_1 + float(x.lastgroup)
+
+    print("Exercise #2 (30 sec)")
+    # Mask sample
+    print("Analyzing sample air.")
+    t_end = time.monotonic() + 30
+    mask_particles_2 = 0
+    matcher = "Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            mask_particles_2 = mask_particles_2 + float(x.lastgroup)
+
+    print("Exercise #3 (30 sec)")
+    # Mask sample
+    print("Analyzing sample air.")
+    t_end = time.monotonic() + 30
+    mask_particles_3 = 0
+    matcher = "Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            mask_particles_3 = mask_particles_3 + float(x.lastgroup)
+
+    print("Exercise #4 (30 sec + 9 sec + purge time)")
+    # Mask sample
+    print("Analyzing sample air.")
+    t_end = time.monotonic() + 30
+    mask_particles_4 = 0
+    matcher = "Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            mask_particles_4 = mask_particles_4 + float(x.lastgroup)
+
+
+
+    # Switch to ambient inlet
+    print("Switching to ambient inlet port.")
+    sio.write("VN\r")
+    sio.flush()
+
+    # Ambient purge
+    print("Purging.")
+    time.sleep(amb_purge_time)
+    #t_end = time.monotonic() + amb_purge_time
+    #while time.monotonic() < t_end:
+    #    # do whatever you do
+
+    # Post-mask ambient sample
+    print("Analyzing ambient air.")
+    t_end = time.monotonic() + 9
+    amb_particles_post = 0
+    matcher="Conc.\\s*(\\d*\\.?\\d*)\\s*#"
+    line = ""
+    while time.monotonic() < t_end:
+        line = sio.readline()
+        sio.flush()
+        x = re.search(matcher,line)
+        if x:
+            amb_particles_post = amb_particles_post + float(x.lastgroup)
+
+    # Switch to mask inlet
+    print("Switching to sample inlet port.")
+    sio.write("VF\r")
+    sio.flush()
+
+    #
+    # Calculate fit factor
+    #
+
+    if mask_particles_1 == 0: mask_particles_1 = 1  # prevent division by zero
+    if mask_particles_2 == 0: mask_particles_2 = 1  # prevent division by zero
+    if mask_particles_3 == 0: mask_particles_3 = 1  # prevent division by zero
+    if mask_particles_4 == 0: mask_particles_4 = 1  # prevent division by zero
+
+    conc_amb_pre = amb_particles_pre / (20 * 1.67)
+    conc_amb_post = amb_particles_post / (9 * 1.67)
+    conc_mask_1 = mask_particles_1 / (30 * 1.67)
+    conc_mask_2 = mask_particles_2 / (30 * 1.67)
+    conc_mask_3 = mask_particles_3 / (30 * 1.67)
+    conc_mask_4 = mask_particles_4 / (30 * 1.67)
+
+    ffactor_1 = (conc_amb_pre + conc_amb_post) / (2 * conc_mask_1)
+    ffactor_2 = (conc_amb_pre + conc_amb_post) / (2 * conc_mask_2)
+    ffactor_3 = (conc_amb_pre + conc_amb_post) / (2 * conc_mask_3)
+    ffactor_4 = (conc_amb_pre + conc_amb_post) / (2 * conc_mask_4)
+    result = [ffactor_1, ffactor_2, ffactor_3, ffactor_4]
+    print("Individual fit factors")
+    print(result)
+    return result
+
+
+
+
 
 ff_exercises = []
-ffactor = 0
-last_amb_conc = None
-for i in range(num_exercises):
-    print("Exercise # "+ str(i + 1) + " of " + str(num_exercises) + "...")
-    if i == 0:
-        ffactor = ft_exercise(last_amb_conc=last_amb_conc, presample_ambient=True)
-        last_amb_conc = ffactor[1]
-    else:
-        ffactor = ft_exercise(last_amb_conc=last_amb_conc, presample_ambient=False)
-    if mode_osha_classic and i == 5:
-        print("Fit factor: " + str(ffactor[0]) + " IGNORED! (grimace exercise)")
-    else:
-        print("Fit factor: " + str(ffactor[0]))
-    ff_exercises.append(ffactor[0])
 
-if mode_osha_classic:
-    ff_exercises.pop(5)
+if mode_osha_modified:
+    print("American CNC Modified QNFT protocol")
+    ff_exercises = ft_osha_modified()
+else:
+    ffactor = 0
+    last_amb_conc = None
+    if mode_osha_classic:
+        print("American CNC Classic QNFT protocol")
+    for i in range(num_exercises):
+        print("Exercise # "+ str(i + 1) + " of " + str(num_exercises) + "...")
+        if i == 0:
+            ffactor = ft_exercise(last_amb_conc=last_amb_conc, presample_ambient=True)
+            last_amb_conc = ffactor[1]
+        else:
+            ffactor = ft_exercise(last_amb_conc=last_amb_conc, presample_ambient=False)
+        if mode_osha_classic and i == 5:
+            print("Fit factor: " + str(ffactor[0]) + " IGNORED! (grimace exercise)")
+        else:
+            print("Fit factor: " + str(ffactor[0]))
+        ff_exercises.append(ffactor[0])
+
+    if mode_osha_classic:
+        ff_exercises.pop(5)
+        print("Individual fit factors")
+        print(ff_exercises)
+
 
 overall_ff = statistics.harmonic_mean(ff_exercises)
+
 print("===========")
 print("OVERALL FIT FACTOR: " + str(overall_ff))
 print("FIT FACTOR THRESHOLD: " + str(ff_pass_level))
